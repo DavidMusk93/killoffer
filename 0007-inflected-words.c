801a7a3f2cbb30892ea51ad8ad76d8eef2ead973
Mingqiang Sun <davidmusksun1993@gmail.com>
2020-06-22 17:24:19 +0800

    [nowcoder] boost inflected words
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- 0007-inflected-words.c
+++ 0007-inflected-words.c
@@ -26,20 +26,47 @@ bool is_inflected_words(const char *p1, const char *p2, size_t n) {
 #undef KEY_TYPE
 }
 
+#define KEY_TYPE unsigned char
+#define ONE_TYPE 1
+#define OTHER_TYPE -1
+
+void record_char(int map[(KEY_TYPE) -1], const char *s, size_t n, int type) {
+    for (size_t i = 0; i < n; ++i) {
+        map[(KEY_TYPE) s[i]] += type;
+    }
+}
+
 int main() {
     int a, b;
-    char *p1, *p2, *data;
     scanf("%d %d", &a, &b);
     if (a != b) {
         puts("false");
         return 0;
     }
+#if 0
+    char *p1, *p2, *data;
     data = malloc(2 * (a + 1));
     p1 = data, p2 = &data[a + 1];
     scanf("%s", p1);
     scanf("%s", p2);
     bool res = is_inflected_words(p1, p2, a);
-    puts(res ? "true" : "false");
     free(data);
+#else
+    char *p = malloc(a + 1);
+    int map[(KEY_TYPE) -1] = {};
+    bool res = true;
+    scanf("%s", p);
+    record_char(map, p, a, ONE_TYPE);
+    scanf("%s", p);
+    record_char(map, p, a, OTHER_TYPE);
+    for (size_t i = 0; i < (KEY_TYPE) -1; ++i) {
+        if (map[i] != 0) {
+            res = false;
+            break;
+        }
+    }
+    free(p);
+#endif
+    puts(res ? "true" : "false");
     return 0;
 }
